a396dd9c73d7f55cddbd1000d0f7e5299b2a9ccf
AliShiri <Your new email>
2023-10-07 14:08:38 -0700

    compelete BottomNavigationView

--- app/src/main/java/com/example/socialmedia/ui/components/BottomNavigationView.kt
+++ app/src/main/java/com/example/socialmedia/ui/components/BottomNavigationView.kt
@@ -1,9 +1,46 @@
 package com.example.socialmedia.ui.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.example.socialmedia.utils.NavigationItem
 
 @Composable
 fun BottomNavigationView(navController: NavController) {
 
+    var selectedRoute by remember { mutableStateOf("Home") }
+
+    val items = listOf(
+        NavigationItem.Home,
+        NavigationItem.Search,
+        NavigationItem.Add,
+        NavigationItem.Activities,
+        NavigationItem.Profile,
+    )
+
+    BottomAppBar {
+        items.forEach {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        painter = painterResource(id = it.icon),
+                        contentDescription = it.title
+                    )
+                },
+                selected = selectedRoute == it.route,
+                onClick = {
+                    navController.navigate(it.route) {
+                        navController.graph.startDestDisplayName.let { route ->
+                            selectedRoute = route
+
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
 }
