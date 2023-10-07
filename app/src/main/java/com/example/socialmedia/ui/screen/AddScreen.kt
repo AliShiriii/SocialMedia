8be81330843689cbbe12e7bbf00e5d58badb8d85
AliShiri <Your new email>
2023-10-07 12:21:29 -0700

    create MainScreen and create Main Screens

---
+++ app/src/main/java/com/example/socialmedia/ui/screen/AddScreen.kt
@@ -0,0 +1,7 @@
+package com.example.socialmedia.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddScreen() {
+}
---
+++ app/src/main/java/com/example/socialmedia/ui/screen/MainScreen.kt
@@ -0,0 +1,67 @@
+package com.example.socialmedia.ui.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.socialmedia.R
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar {
+                Text(text = stringResource(id = R.string.app_name))
+            }
+        },
+        bottomBar = {
+
+//            BottomNavigationView(navController)
+
+            BottomAppBar(
+                cutoutShape = MaterialTheme.shapes.small.copy(
+                    CornerSize(percent = 50)
+                )
+            ) {
+
+            }
+        }
+    ) {
+        NavHost(
+            navController = navController,
+            startDestination = "Home"
+        ) {
+            composable("Home") {
+
+                HomeScreen()
+            }
+            composable("Search") {
+                SearchScreen()
+            }
+            composable("Add") {
+                AddScreen()
+            }
+            composable("Activities") {
+                ActivitiesScreen()
+            }
+            composable("Profile") {
+                ProfileScreen()
+            }
+        }
+    }
+}
