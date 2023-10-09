ff4b9c32a54e5066d99f310060347ecd607ded50
AliShiri <Your new email>
2023-10-09 12:39:26 -0700

    add and compeleted aTopAppBar

--- app/src/main/java/com/example/socialmedia/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/screen/MainScreen.kt
@@ -11,13 +11,16 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.socialmedia.R
 import com.example.socialmedia.ui.components.BottomNavigationView
+import com.example.socialmedia.ui.components.TopAppView
 
+@Preview(showBackground = true)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
@@ -27,7 +30,8 @@ fun MainScreen() {
     Scaffold(
         topBar = {
             TopAppBar {
-                Text(text = stringResource(id = R.string.app_name))
+//                Text(text = stringResource(id = R.string.app_name))
+                TopAppView()
             }
         },
         bottomBar = {
@@ -38,9 +42,7 @@ fun MainScreen() {
                 cutoutShape = MaterialTheme.shapes.small.copy(
                     CornerSize(percent = 50)
                 )
-            ) {
-
-            }
+            ) {}
         }
     ) {
         NavHost(
@@ -48,7 +50,6 @@ fun MainScreen() {
             startDestination = "Home"
         ) {
             composable("Home") {
-
                 HomeScreen()
             }
             composable("Search") {
