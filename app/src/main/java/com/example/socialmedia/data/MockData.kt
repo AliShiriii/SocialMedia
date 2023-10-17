232ce2d317d0eca0510f72a99b95b33ddf4e8ce5
AliShiri <Your new email>
2023-10-17 12:45:14 -0700

    crreate object of post and users in MockDat\a

--- app/src/main/java/com/example/socialmedia/data/MockData.kt
+++ app/src/main/java/com/example/socialmedia/data/MockData.kt
@@ -1,11 +1,15 @@
 package com.example.socialmedia.data
 
+import com.example.socialmedia.models.Post
 import com.example.socialmedia.models.Story
+import com.example.socialmedia.models.User
 
 
 object MockData {
 
     val stories = ArrayList<Story>()
+    val users = ArrayList<User>()
+    val posts = ArrayList<Post>()
 
     init {
         stories.add(
@@ -76,6 +80,59 @@ object MockData {
                 "Ali Shiri",
                 "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
                 false
+            )
+        )
+
+        ///Users
+        users.add(
+            User(
+                "Ali",
+                "Ali Shiri",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+            )
+        )
+
+        users.add(
+            User(
+                "AmirHossein",
+                "AmirHossein Shiri",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+            )
+        )
+
+        users.add(
+            User(
+                "Mahan",
+                "Mahan Barkokrazm",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+            )
+        )
+
+        //posts
+        posts.add(
+            Post(
+                users[0],
+                "Tehran, Iran",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+                "Tehran is one of the iran cities, tehran is a good city, tehran is a big city and beautiful."
+            )
+        )
+
+        posts.add(
+            Post(
+                users[1],
+                "Vancover, Canada",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+                "Vancover is one of the canada cities, Vancover is a good city, Vancover is a big city and beautiful."
+            )
+        )
+
+        posts.add(
+            Post(
+                users[2],
+                "Torento, Canada",
+                "https://www.instagram.com/reel/CyWTDZmKLky/?igshid=MzRlODBiNWFlZA==",
+                "Torento is one of the canada cities, Torento is a good city, Torento is a big city and beautiful."
             )
         )
     }
